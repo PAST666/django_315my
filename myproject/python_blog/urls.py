6197fba1d3081a0c7e687dc7a7335989f0c78008
alex6 <alex66651@yandex.ru>
2024-06-29 22:47:18 +0800

    подключили и создали static

--- myproject/python_blog/urls.py
+++ myproject/python_blog/urls.py
@@ -1,9 +1,7 @@
-from django.contrib import admin
 from django.urls import path
 from python_blog import views
 
 urlpatterns = [
-    path("", views.main),
     path("category/", views.category, name="categories"),
     path("category/<int:category_id>/", views.category_detail, name="category"),
 ]
