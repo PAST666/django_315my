ce7440d62f1b8cf574d66b490a817014beb15a32
alex6 <alex66651@yandex.ru>
2024-07-20 11:43:45 +0800

    создали суперадмина+добавили 3 поста через shell

--- myproject/blog/models.py
+++ myproject/blog/models.py
@@ -1,6 +1,38 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.utils.text import slugify
+from unidecode import unidecode
+
 
 class Post(models.Model):
+    """
+    Модель поста
+    """
+
     title = models.CharField(max_length=200)
     text = models.TextField()
-# Create your models here.
+    slug = models.SlugField(unique=True)
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    data = models.JSONField(null=True, blank=True, default=list)
+    published_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    def save(self, *args, **kwargs):
+        """
+        Переопределение метода save для автоматической генерации slug
+        """
+        if not self.slug or self.slug == "":
+            self.slug = slugify(unidecode(self.title))
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        """
+        Строковое представление модели
+        """
+        return f"{self.title}: {self.slug}"
+
+    def get_absolute_url(self):
+        """
+        Метод для получения абсолютного URL поста
+        """
+        return f"/blog/{self.slug}/"
